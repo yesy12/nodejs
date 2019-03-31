cd23de01fa09c5a5ad34c3c2164b0824a081bc3f
Alison <alisonvieira728@yahoo.com.br>
2019-03-30 21:30:52 -0300

    edita categorias e deu ruim no req.flash

--- aula31/routes/admin.js
+++ aula31/routes/admin.js
@@ -70,6 +70,7 @@ router.post("/categorias/nova",function(req,res){
     new Categoria(novaCategoria).save().//Salvando no Db
     then(function(){
       req.flash('success_msg',"Categoria salva com sucesso");
+      //console.log(req.flash("success_msg"))
     }).catch(function(error){
       req.flash("error_msg","Houve um erro ao criar a categoria,tente novamente mais tarde")
       console.log("erro: "+error)
@@ -80,4 +81,62 @@ router.post("/categorias/nova",function(req,res){
   }
 })
 
-module.exports = router
+router.get("/categorias/editar/:id",function(req,res){
+  Categoria.findOne({
+    _id:req.params.id
+  }).then(function(categoria){
+    res.render("admin/edit-categorias",{
+      categoria:categoria
+    })
+  })
+  
+})
+
+router.post("/categorias/edit",function(req,res){
+  
+  const nome = req.body.nome
+  const slug = req.body.slug
+  
+  var erros = [];
+  
+  if(!nome || typeof nome == undefined || nome == null){
+    erros.push({
+      texto:"Nome Inválido"
+    })
+  }
+  
+  if(nome.length >0 && nome.length< 3){
+    erros.push({
+      texto:"Nome da categoria muito pequeno"
+    })
+  }
+  
+  if(!slug || typeof slug == undefined || slug == null){
+    erros.push({
+      texto:"Slug Inválido"
+    })
+  }
+  
+  if(erros.length > 0){
+    res.render("admin/add-categorias",{
+      erros,
+    })
+  }
+  else{ 
+    Categoria.findOne({
+      _id:req.body.id
+    }).then(function(categoria){
+      categoria.nome = nome
+      categoria.slug = slug
+      
+      req.flash("success_msg","Editado com sucesso")
+    }).catch(function(error){
+      req.flash("error_msg","Houve um erro ao editar")
+    })
+    
+    res.redirect("/admin/categorias");
+
+  }
+})
+
+module.exports = router;
